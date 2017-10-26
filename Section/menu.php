8f5013ef84a57075c5ef71d5713c955d95fcd080
Carlos Octavio Preciado Lafarga <carloslafarga2244@gmail.com>
2017-10-26 11:49:55 -0700

    Correciones de acomodo menu.
    Modifique un poco nomas naun de el acomodo de las etiquetas.

--- Section/menu.php
+++ Section/menu.php
@@ -1,5 +1,4 @@
- <div id="wrapper">
-        <div id="page-wrapper" class="gray-bg">
+
         <div class="row border-bottom white-bg">
         <nav class="navbar navbar-static-top" role="navigation">
             <div class="navbar-header">
@@ -10,19 +9,25 @@
             </div>
             <div class="navbar-collapse collapse" id="navbar">
                 <ul class="nav navbar-nav">
-                    <li class="dropdown">
-                        <a aria-expanded="false" role="button" href="#" class="dropdown-toggle" data-toggle="dropdown"> Menu <span class="caret"></span></a>
+                    
+                           
+
+                        <li class="dropdown">
+                        <a aria-expanded="false" role="button" href="#" class="dropdown-toggle" data-toggle="dropdown"> Funciones <span class="caret"></span></a>
                         <ul role="menu" class="dropdown-menu">
-                           <h2>
-                           <li><i class="fa fa-shopping-cart"> <a href="" > Venta</a></i></li><br>
-                            <li><i class="fa fa-cubes"> <a href="almacen.php" > Almacen</a></i></li><br>
-                            <li><i class="fa fa-user-circle-o"> <a href="usuarios.php" > Usuarios</a></i></li>
-                            </h2>
+                            <li><a href=""><i class="fa fa-shopping-cart"></i>&nbsp;&nbsp;Venta</a></li>
+                            <li><a href="almacen.php"><i class="fa fa-cubes"></i>&nbsp;&nbsp;Almacen</a></li>
+                            <li><a href="usuarios.php"><i class="fa fa-user-circle-o"></i>&nbsp;&nbsp;Usuarios</a></li>
+                            
                         </ul>
                     </li>
+                        
 
                 </ul>
                 <ul class="nav navbar-top-links navbar-right">
+                    <li>
+                       <b> Bienvenido:</b>&nbsp;&nbsp;<?php echo $_SESSION["nombre"];?>
+                    </li>
                     <li>
                         <a href="../Controlador/Salir.php">
                             <i class="fa fa-sign-out"></i> Cerrar Sesion
