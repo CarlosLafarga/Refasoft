102f1376ef64d18a07b2215e114aadb9e3d78d29
sabori80 <44326584+sabori80@users.noreply.github.com>
2018-12-29 13:58:21 -0700

    Update vales.php

--- Vista/vales.php
+++ Vista/vales.php
@@ -46,9 +46,37 @@ if (!isset($_SESSION["nombre"])) {
                                      <h5>Productos Faltantes</h5>
                                   </div>
 
+
+
+                                  <!-- LO DE LA FECHA -->
+                                    <div class="ibox-content">
+                                  <div class="row">
+                                  <div class="col-lg-3">
+                                 <label>Del:</label>
+                                 <div class="form-group" id="data_1">
+                                 <div class="input-group date">
+                                     <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                     <input type="text"  name="de" id="de" value = "<?php echo date('m/d/Y'); ?>" class="form-control" >
+                                 </div>
+                                 </div>
+                                 </div>
+                                 
+
+                                 <div class="col-lg-12">
+                                 <button class="btn btn-primary" id="consultar_vale" name="consultar_vale">Consultar</button>
+                               <!--   <button class="btn btn-info" id="imprimir" name="consultar">Imprimir</button> -->
+                                 </div>
+                                   </div>
+                                   </div>
+                                 <!-- HATA AQUI LO DE LA FECHA -->
+
+
+
+
+
                                  <div class="ibox-content">
                                  <div class="table-responsive">
-                                   <table  id="faltantes" class="table table-striped table-bordered table-hover dataTables-example">
+                                   <table  id="vale_chalio" class="table table-striped table-bordered table-hover dataTables-example">
                                    <thead>
                                    <tr>
                                        <th>ID</th>
