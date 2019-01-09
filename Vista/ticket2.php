aaeffa0ad3f1e32cd314ad0f9b88bf59337b6748
sabori80 <44326584+sabori80@users.noreply.github.com>
2019-01-09 15:42:44 -0700

    Update ticket2.php

--- Vista/ticket2.php
+++ Vista/ticket2.php
@@ -7,18 +7,18 @@ if (isset($_GET['no_ticket'])) {
     $ticket = $_GET['no_ticket'];
 }
 $cn       = Conectarse();
-$sql      = "SELECT * FROM venta_articulos WHERE no_tiket = '" . $ticket . "';";
+$sql      = "SELECT venta_articulos.*, productos2.Grupo FROM venta_articulos left join productos2 ON venta_articulos.codigo = productos2.codigo WHERE no_tiket = '" . $ticket . "';";
 $ejecutar = mysql_query($sql, $cn);
 
 
-$pdf = new FPDF($orientation='P',$unit='mm', array(45,350));
+$pdf = new FPDF($orientation='P',$unit='mm', array(70,350)); // el primer nuemero del array es el ancho de pdf aqui
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',8);    //Letra Arial, negrita (Bold), tam. 20
 $textypos = 5;
 $pdf->setY(2);
 $pdf->setX(7);
 $pdf->Cell(10,$textypos,"Refaccionaria Rivera");
-$pdf->SetFont('Arial','',5);    //Letra Arial, negrita (Bold), tam. 20}
+$pdf->SetFont('Arial','',6);    //Letra Arial, negrita (Bold), tam. 20} aqui
 $pdf->setY(6);
 $pdf->setX(8);
 $pdf->Cell(20,$textypos,"Boulevar Solidaridad 601\tChoyal,");
@@ -48,12 +48,23 @@ while($array= mysql_fetch_array($ejecutar)){
 $pdf->setX(1);
 $pdf->Cell(5,$off,$array["cantidad"]);
 $pdf->setX(8);
-$pdf->Cell(40,$off,  strtoupper(substr($array["codigo"], 0,12)) );
+$pdf->Cell(40,$off,  strtoupper(substr($array["codigo"]." ", 0,12)) );
 $pdf->setX(20);
 $pdf->Cell(11,$off,  "$".number_format($array["precio"],2,".",",") ,0,0,"R");
 $pdf->setX(32);
 $pdf->Cell(11,$off,  "$ ".number_format($array["total"],2,".",",") ,0,0,"R");
 $total += $array["cantidad"]*$array["precio"];
+$off+=6;
+
+$pdf->setX(1);
+$pdf->Cell(5,$off,"");
+$pdf->setX(8);
+$pdf->Cell(40,$off,  strtoupper(substr($array["Grupo"], 0,12)) );
+$pdf->setX(20);
+$pdf->Cell(11,$off,  "");
+$pdf->setX(32);
+$pdf->Cell(11,$off,  "");
+
 $off+=6;
 
 }
