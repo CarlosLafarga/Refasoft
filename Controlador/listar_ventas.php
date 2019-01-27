d3c57a8adbf337143706c45ea04ae8f748b65394
sabori80 <44326584+sabori80@users.noreply.github.com>
2019-01-27 16:31:37 -0700

    Update listar_ventas.php

--- Controlador/listar_ventas.php
+++ Controlador/listar_ventas.php
@@ -3,7 +3,7 @@ include "../conect/conexion.php";
 $cn    = Conectarse();
 $fecha = $_GET['fecha'];
 
-$select = "SELECT venta_articulos.*,ventas.tipo_pago FROM venta_articulos LEFT JOIN ventas ON venta_articulos.no_tiket = ventas.no_tiket
+$select = "SELECT venta_articulos.*,ventas.tipo_pago FROM venta_articulos LEFT JOIN ventas ON venta_articulos.no_tiket = ventas.no_tiket 
 WHERE DATE(venta_articulos.fecha_venta) =  '" . $fecha . "' AND tipo_pago !='credito'  AND ventas.cancelado != 1 AND venta_articulos.devolucion !=1 ORDER BY no_tiket ";
 
 $result = mysql_query($select, $cn);
