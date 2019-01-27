a8254fa77f2fdb7fbb38a1fc3b043de4c257a5c3
sabori80 <44326584+sabori80@users.noreply.github.com>
2019-01-27 16:31:46 -0700

    Update listar_ventas_credito.php

--- Controlador/listar_ventas_credito.php
+++ Controlador/listar_ventas_credito.php
@@ -4,7 +4,7 @@ $cn    = Conectarse();
 $fecha = $_GET['fecha'];
 
 $select = "SELECT venta_articulos.*,ventas.tipo_pago FROM venta_articulos LEFT JOIN ventas ON venta_articulos.no_tiket = ventas.no_tiket
-WHERE DATE(venta_articulos.fecha_venta) =  '" . $fecha . "' AND tipo_pago ='credito'  AND ventas.cancelado != 1 AND venta_articulos.devolucion != 1 AND  ORDER BY no_tiket ";
+WHERE DATE(venta_articulos.fecha_venta) =  '" . $fecha . "' AND tipo_pago ='credito'  AND ventas.cancelado != 1 AND venta_articulos.devolucion != 1  ORDER BY no_tiket ";
 
 $result = mysql_query($select, $cn);
 
